279222f701a932c0b414e375a69a25be25d0d499
Ridwan Sarder <ridwanzuraiz@gmail.com>
2025-05-12 22:31:57 +0600

    bug fixes

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,13 +1,16 @@
 import ProductDetails from "@/components/productDetails";
 import { stripe } from "@/lib/stripe";
 
-export default async function ProductPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+interface ProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function ProductPage({ params }: ProductPageProps) {
   const product = await stripe.products.retrieve(params.id, {
     expand: ["default_price"],
   });
-  return <ProductDetails product={product}/>;
+
+  return <ProductDetails product={product} />;
 }
